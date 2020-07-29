cecc74340b85b0a2e1cecb15c14b1b2e5b20bff1
unknown <hzh_innovation@163.com>
2020-07-30 00:57:26 +0800

    [hzh] feat: finish execrise_07

--- src/exercise_06/vector.js
+++ src/exercise_06/vector.js
@@ -12,5 +12,32 @@ export default class Vector {
   // * Please do NOT modify the signature of the class. Please put all your code in the Vector
   // class.
 
-  // PLEASE DELETE THIS LINE AND ADD YOUR IMPLEMENTATION HERE
+  constructor(xNum, yNum) {
+    this.xNum = xNum;
+    this.yNum = yNum;
+  }
+
+  get x() {
+    return this.xNum;
+  }
+
+  get y() {
+    return this.yNum;
+  }
+
+  static plus(vector1, vector2) {
+    let finalX = vector1.x + vector2.x;
+    let finalY = vector1.y + vector2.y;
+    return new Vector(finalX, finalY);
+  }
+
+  static minus(vector1, vector2) {
+    let finalX = vector1.x - vector2.x;
+    let finalY = vector1.y - vector2.y;
+    return new Vector(finalX, finalY);
+  }
+
+  distance() {
+    return Math.sqrt(Math.pow(this.xNum, 2) + Math.pow(this.yNum, 2));
+  }
 }
