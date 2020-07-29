4af7652b308f8832827d16a57237b844e7d53cd8
unknown <hzh_innovation@163.com>
2020-07-30 00:29:57 +0800

    [hzh] feat: finish execrise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,23 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  }
+
+  let result = [];
+
+  if (!array.length) {
+    return result;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if ((typeof (array[i])) === 'number') {
+      result.push(array[i]);
+    } else {
+      result = result.concat(array[i]);
+    }
+  }
+
+  return result;
 }
