5a0a0560612f49a84f304c57850276120f1dabf2
unknown <hzh_innovation@163.com>
2020-07-29 23:29:01 +0800

    [hzh] feat: finish exercise_02

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,20 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (string === undefined || string === null) {
+    return 0;
+  }
+
+  if (prediction === undefined || prediction === null) {
+    return string.length;
+  } 
+
+  let count = 0;
+  for (let i = 0; i < string.length; i += 1) {
+    if (prediction(string[i])) {
+      console.log('进来了');
+      count += 1;
+    }
+  }
+  return count;
 }
