14d5bfb273322b87ff8527e2e5402146bb74630d
unknown <hzh_innovation@163.com>
2020-07-30 00:13:39 +0800

    [hzh] feat: finish exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,32 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (!array.length) {
+    throw new Error('Creating list from empty array');
+  }
+
+  function Node(value) {
+    this.value = value;
+    this.next = null;
+  }
+
+  if (array.length == 1) {
+    return new Node(array[0]);
+  }
+
+  function link(node1, node2) {
+    node1.next = node2;
+  }
+
+  let head = new Node(array[0]);
+  let cur = head;
+  for (let i = 0; i < array.length - 1; i++) {
+    cur.next = new Node(array[i+1]);
+    cur = cur.next;
+  }
+
+  return head;
 }
